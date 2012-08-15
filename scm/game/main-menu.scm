0fa1d774d1da918f4f29f6d0b02c40617c0145f9
Krister Svanlund <krister.svanlund@gmail.com>
2012-08-15 16:52:21 +0200

    Added a color-theme to the main-menu

--- scm/game/main-menu.scm
+++ scm/game/main-menu.scm
@@ -11,40 +11,51 @@
              (ninja menu))
 
 (define (draw-main-menu scr)
-  (clear scr)
-  (touchwin scr)
-  (refresh scr)
-  (let ((menu (make-ninja-menu "Ducking ninja!" (default-menu-color-theme)))) 
-    (debug "Setup menu\n") 
+  (let* ((theme (make-ninja-menu-color-theme
+                  0  #| foreground |#
+                  0  #| background |#
+                  15 #| title      |#
+                  15 #| prompt     |#
+                  15 #| enabled    |#
+                  8  #| disabled   |#
+                  7  #| short      |#
+                  8  #| bracket    |#
+                  ))
+         (menu (make-ninja-menu "Ducking ninja!" theme))) 
     (add-item! menu "New Game" #\n 'new-game #t) 
     (add-item! menu "Options" #\o 'show-options #t) 
     (add-item! menu "Color test" #\c 'color-test #t) 
     (add-item! menu "Help" #\h 'show-help #f)
     (add-blank! menu) 
     (add-item! menu "Quit" #\q 'quit #t) 
-    (let redraw-loop
-      ((mx   (getmaxx scr)) 
-       (my   (getmaxy scr))) 
-      (match (catch 'window-resize
-               (λ () (do-menu menu scr (- my 2) (- mx 2) 1 1))
-               (λ (key . args)
-                  (redraw-loop (getmaxx scr) (getmaxy scr))))
-        ('new-game 
-         ((@ (game game-screen) start-game) scr)
-         (draw-main-menu scr))
-        ('show-options 
-         (addstr scr "Do the option screen")
-         (get-char scr)
-         (draw-main-menu scr))
-        ('color-test 
-         ((@ (util colors) draw-test-screen) scr)
-         (draw-main-menu scr))
-        ('show-help 
-         (addstr scr "There is no help-screen")
-         (get-char scr)
-         (draw-main-menu scr))
-        ('quit (addstr scr "I'm leaving now!!"))
-        (_ #f)))))
+    (let redraw-menu ((state #f))
+      (clear scr)
+      (touchwin scr)
+      (refresh scr)
+      (debug "Setup menu\n") 
+      (let redraw-loop
+        ((mx   (getmaxx scr)) 
+         (my   (getmaxy scr))) 
+        (match (catch 'window-resize
+                 (λ () (do-menu menu scr (- my 2) (- mx 2) 1 1))
+                 (λ (key . args)
+                    (redraw-loop (getmaxx scr) (getmaxy scr))))
+          ('new-game 
+           ((@ (game game-screen) start-game) scr)
+           (redraw-menu state))
+          ('show-options 
+           (addstr scr "Do the option screen")
+           (get-char scr)
+           (redraw-menu state) )
+          ('color-test 
+           ((@ (util colors) draw-test-screen) scr)
+           (redraw-menu state) )
+          ('show-help 
+           (addstr scr "There is no help-screen")
+           (get-char scr)
+           (redraw-menu state) )
+          ('quit (addstr scr "I'm leaving now!!"))
+          (_ #f))))))
 
 (define (run stdscr)
   (debug "This is where we setup the game\n")
